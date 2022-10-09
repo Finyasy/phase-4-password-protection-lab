5c720960fc81454d61d0da8a77295e67a225ef87
Bryan Bosire <bryanbosire@gmail.com>
2022-10-10 00:01:37 +0300

    commit tree 18b271fbfe9367708d353a0bd68261e8133b273b

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  # POST /login
+  post '/login', to: 'sessions#create'
+  # DELETE /logout
+  delete '/logout', to: 'sessions#destroy'
+  # POST /signup
+  post '/signup', to: 'users#create'
+  # GET /me
+  get '/me', to: 'users#show'
+
 end
